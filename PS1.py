5aa4f8baf7e985fb39b67bca4c4bc218c1f0234a
hanksf <fergaldeclanhanks@gmail.com>
2019-11-01 11:38:43 -0500

    wrote estimates generator for sims uhlig

--- PS1.py
+++ PS1.py
@@ -5,7 +5,17 @@ import scipy as sci
 
 #%%
 #Question 1 functions definitions
+#holy shit the loops
 def Sims_Uhlig(n_sims=10000, series_length=100, true_prior = [0.8, 1.1, 31],y_0=0,sig_epsilon):
     rho_grid = np.linspace(true_prior[0],true_prior[1],true_prior[2])
-    epsilon = np.random.normal(size=(n_sims,series_length))
-    
+    epsilon = np.random.normal(size=(series_length,n_sims))
+    estimates = np.zeros((rho_grid.size,n_sims))
+    for i in range(rho_grid.size):
+        y_matrix = np.zeros(series_length+1,n_sims)
+        rho = rho_grid[i]
+        y_matrix[1,:] = epsilon[0,:]
+        for j in range(series_length-1):
+            y_matrix[j+2,:] = rho*y_matrix[j+1,:]+epsilon[j+1,:]
+    estimates[i,:] = np.diagonal(y_matrix[1:].T@y_matrix[:100])/np.diagonal(y_matrix[:100].T@y_matrix[:100])
+    return estimates
+
