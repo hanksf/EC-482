253b557fce85d8ddc7f2532280e4ae67aeb54d03
hanksf <fergaldeclanhanks@gmail.com>
2019-11-13 14:58:24 -0600

    imported excel file

--- PS2_output.py
+++ PS2_output.py
@@ -3,6 +3,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 import scipy as sci
 import PS2_func
+import pandas as pd
+
 
 ## Question 1
-
+## columns are ouput, prices, unemployment, Pcom, M2, interest rate
+df = pd.read_excel(r'SZdata.xlsx')
+Data = df.as_matrix()
+print(Data)
